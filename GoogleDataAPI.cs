75e4f1e1ade754be1fead26a6b3f1b7c17529645
Joaquim Cardeira <joaquim@cardeira.com>
2012-12-17 02:56:03 +0000

    try catch

--- GoogleDataAPI.cs
+++ GoogleDataAPI.cs
@@ -14,23 +14,29 @@ namespace cardeira.p2s
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public GDataAPI(NameValueCollection parameters)
         {
-
-            SpreadsheetsService service = new SpreadsheetsService("post2spreadsheet");
-            service.setUserCredentials(cardeira.Properties.Settings.Default.gUserName, cardeira.Properties.Settings.Default.gPassword);
-
-            Google.GData.Spreadsheets.SpreadsheetQuery query = new Google.GData.Spreadsheets.SpreadsheetQuery();
-
-            SpreadsheetFeed feed = service.Query(query);
-            SpreadsheetEntry entry = null;
-            foreach (SpreadsheetEntry e in feed.Entries)
+            try
             {
-                entry = e;
-                logger.Debug("Spreadsheet: {0}", entry.Title.Text);
-                if (entry.Title.Text == cardeira.Properties.Settings.Default.spreadsheetkey)
-                    break;
+                SpreadsheetsService service = new SpreadsheetsService("post2spreadsheet");
+                service.setUserCredentials(cardeira.Properties.Settings.Default.gUserName, cardeira.Properties.Settings.Default.gPassword);
+
+                Google.GData.Spreadsheets.SpreadsheetQuery query = new Google.GData.Spreadsheets.SpreadsheetQuery();
+
+                SpreadsheetFeed feed = service.Query(query);
+                SpreadsheetEntry entry = null;
+                foreach (SpreadsheetEntry e in feed.Entries)
+                {
+                    entry = e;
+                    logger.Debug("Spreadsheet: {0}", entry.Title.Text);
+                    if (entry.Title.Text == cardeira.Properties.Settings.Default.spreadsheetkey)
+                        break;
+                }
+                if (entry != null)
+                    InsertRow(service, (WorksheetEntry)entry.Worksheets.Entries[0], parameters);
             }
-            if (entry!=null)            
-                InsertRow(service, (WorksheetEntry)entry.Worksheets.Entries[0], parameters);                
+            catch (Exception e)
+            {
+                logger.ErrorException("error writing to spreadsheet", e);
+            }
         }
 
         private static ListEntry InsertRow(SpreadsheetsService service, WorksheetEntry entry, NameValueCollection parameters)
@@ -50,6 +56,12 @@ namespace cardeira.p2s
                 curElement.LocalName = key;
                 newRow.Elements.Add(curElement);
             }
+
+            // add datetime
+            ListEntry.Custom el = new ListEntry.Custom();
+            el.Value = parameters["data"];
+            el.LocalName = DateTime.Now.ToString() ;
+            newRow.Elements.Add(el);
 
             ListEntry insertedRow = feed.Insert(newRow);
             return insertedRow;
